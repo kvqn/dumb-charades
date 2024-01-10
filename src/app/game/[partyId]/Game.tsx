0b54529fee502513680d0fd2780cbd2c21e57989
kvqn <guneetaggarwal@gmail.com>
2024-01-10 16:22:09 +0530

    feat: all events appear in the chat

--- src/app/game/[partyId]/Game.tsx
+++ src/app/game/[partyId]/Game.tsx
@@ -12,12 +12,30 @@ function max(a: number, b: number) {
   return a > b ? a : b
 }
 
+type ChatMessage = {
+  createdAt: Date
+  event:
+    | ({
+        type: "ChatEvent"
+      } & Prisma.ChatEventGetPayload<{ include: { user: true } }>)
+    | ({
+        type: "PartyCreateEvent"
+      } & Prisma.PartyCreateEventGetPayload<{ include: { User: true } }>)
+    | ({
+        type: "PartyDestroyEvent"
+      } & Prisma.PartyDestroyEventGetPayload<{ include: { User: true } }>)
+    | ({
+        type: "UserEnterEvent"
+      } & Prisma.UserEnterEventGetPayload<{ include: { user: true } }>)
+    | ({
+        type: "UserLeaveEvent"
+      } & Prisma.UserLeaveEventGetPayload<{ include: { user: true } }>)
+}
+
 export function Game({ partyId, user }: { partyId: string; user: User }) {
   const [members, setMembers] = useState<Prisma.UserGetPayload<object>[]>([])
   const [leaderId, setLeaderId] = useState<string | null>(null)
-  const [chatEvents, setChatEvents] = useState<
-    Prisma.ChatEventGetPayload<{ include: { user: true } }>[]
-  >([])
+  const [chatEvents, setChatEvents] = useState<ChatMessage[]>([])
 
   const [gameDestroyed, setGameDestroyed] = useState(false)
 
@@ -28,9 +46,7 @@ export function Game({ partyId, user }: { partyId: string; user: User }) {
   useEffect(() => {
     let lastEventId = -1
     const members = new Map<string, Prisma.UserGetPayload<object>>()
-    const chatEvents: Prisma.ChatEventGetPayload<{
-      include: { user: true }
-    }>[] = []
+    const _chatEvents: typeof chatEvents = []
     async function _() {
       while (true) {
         const events = await getPartyEvents(partyId, lastEventId)
@@ -44,25 +60,51 @@ export function Game({ partyId, user }: { partyId: string; user: User }) {
             )
             setLeaderId(event.PartyCreateEvent.userId)
             setMembers([...members.values()])
+            _chatEvents.push({
+              createdAt: event.createdAt,
+              event: { type: "PartyCreateEvent", ...event.PartyCreateEvent },
+            })
+            setChatEvents(_chatEvents)
           }
           if (event.PartyDestroyEvent) {
+            _chatEvents.push({
+              createdAt: event.createdAt,
+              event: { type: "PartyDestroyEvent", ...event.PartyDestroyEvent },
+            })
             setGameDestroyed(true)
           }
           if (event.ChatEvent) {
             if (event.ChatEvent.user.id === user.id) setSendingMessage(false)
-            chatEvents.push(event.ChatEvent)
-            setChatEvents(chatEvents)
+            _chatEvents.push({
+              createdAt: event.createdAt,
+              event: { type: "ChatEvent", ...event.ChatEvent },
+            })
           }
           if (event.UserEnterEvent) {
             members.set(event.UserEnterEvent.user.id, event.UserEnterEvent.user)
             setMembers([...members.values()])
+            _chatEvents.push({
+              createdAt: event.createdAt,
+              event: {
+                type: "UserEnterEvent",
+                ...event.UserEnterEvent,
+              },
+            })
           }
           if (event.UserLeaveEvent) {
             members.delete(event.UserLeaveEvent.user.id)
             setMembers([...members.values()])
+            _chatEvents.push({
+              createdAt: event.createdAt,
+              event: {
+                type: "UserLeaveEvent",
+                ...event.UserLeaveEvent,
+              },
+            })
           }
           lastEventId = max(lastEventId, event.id)
         }
+        setChatEvents(_chatEvents)
         await sleep(5000)
       }
     }
@@ -91,11 +133,7 @@ export function Game({ partyId, user }: { partyId: string; user: User }) {
       </div>
       <div>
         Chat:
-        {chatEvents.map((event, idx) => (
-          <div key={idx}>
-            {event.user.name}: {event.message}
-          </div>
-        ))}
+        <ChatMessages chatEvents={chatEvents} />
         {sendingMessage ? <div>Sending message...</div> : null}
         <input className="border" type="text" ref={chatBoxRef} />
         <button
@@ -115,6 +153,46 @@ export function Game({ partyId, user }: { partyId: string; user: User }) {
           Send
         </button>
       </div>
+    </div>
+  )
+}
+
+function Message({ message }: { message: ChatMessage }) {
+  if (message.event.type === "ChatEvent") {
+    return (
+      <div>
+        {message.event.user.name}: {message.event.message}
+      </div>
+    )
+  }
+
+  if (message.event.type === "PartyCreateEvent") {
+    return <div>{message.event.User.name} created the party</div>
+  }
+
+  if (message.event.type === "PartyDestroyEvent") {
+    return <div>Party destroyed</div>
+  }
+
+  if (message.event.type === "UserEnterEvent") {
+    return <div>{message.event.user.name} entered the party</div>
+  }
+
+  if (message.event.type === "UserLeaveEvent") {
+    return <div>{message.event.user.name} left the party</div>
+  }
+
+  return null
+}
+
+function ChatMessages({ chatEvents }: { chatEvents: ChatMessage[] }) {
+  return (
+    <div>
+      {chatEvents.map((event, idx) => (
+        <div key={idx}>
+          <Message message={event} />
+        </div>
+      ))}
     </div>
   )
 }
